1b5929947ffe11bf21cb820a57a04b982a4bfc90
junsin <a01075990473@gmail.com>
2026-07-09 00:35:08 +0900

    프론트엔드 링크 반영 수정

--- backend/test.py
+++ backend/test.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 # 1. CORS 설정 (실제 프론트엔드 주소로 변경하세요)
 allow_origins = [
-    "http://localhost:3000",  # 로컬 테스트용
+    # "http://localhost:3000",  # 로컬 테스트용
     "https://teammallang.github.io/MallangTest/" # 실제 배포용
 ]
 
