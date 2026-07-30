cf492629a6ba2204662f5aa489bd9a4382dd2391
Frenn14 <implan9102@gmail.com>
2026-07-31 03:14:58 +0900

    백엔드 서버 변경

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  API_BASE: "https://mallangtest.onrender.com",
+  API_BASE: "https://mallang-67cb.onrender.com",
   REGIONS: [
     "서울", "경기", "인천", "부산", "대구", "대전", "광주", "울산",
     "세종", "강원", "충북", "충남", "전북", "전남", "경북", "경남", "제주"
